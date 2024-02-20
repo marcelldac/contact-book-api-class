f6611ecad8dcf926f854116a67941d9e01003c46
dacmarcell <devmarcell@protonmail.com>
2024-02-20 00:13:48 -0300

    style: removes entity comments

--- src/index.js
+++ src/index.js
@@ -8,25 +8,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-// MODEL
-//
-// contact = {
-//  id: string,
-//  name: string,
-//  phone: string,
-//  isActive: string,
-//  updatedAt: string | null
-//  createdAt: string
-// }
-//
-
-// ENUM
-//
-// enum statusCode {
-//  Ok = 200,
-//  Created = 201
-// }
-
 app.use(contactRouter);
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
